3a2df6269d9fbab6c5d5ed6674a906c10b670e25
iguesspaul <paul.l.vantilburg@gmail.com>
2023-05-14 15:34:03 -0400

    hello

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let color: string = "";
+    let backgroundcolor: string = "";
+    let sizes: Array<string> = ["tiny", "scriptsize", "footnotesize", "small", "normalsize", "large", "Large", "LARGE", "huge", "Huge"]; 
+    let size: string = "";
+    let result = `\\${size} \\color{${color}} \\colorbox{${backgroundcolor}} { text }`;
+</script>
+
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+        border: 0;
+        display: block;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 1rem; 
+    }
+
+    select {
+        border: 3px solid black;
+        border-radius: 10%;
+    }
+</style>
+
+<div class="container">
+    <p> Text Color </p>
+    <input type="color" bind:value={color} />
+    
+    <p> Background Color </p>
+    <input type="color" bind:value={backgroundcolor} />
+    
+    <p> Size </p>
+    <select bind:value={size}> 
+        {#each sizes as i}
+            <option value={i}>
+                {i}
+            </option>
+        {/each}
+    </select>
+
+
+    <p>Result:</p>
+    <code>{result}</code>
+</div>
+
+
+
